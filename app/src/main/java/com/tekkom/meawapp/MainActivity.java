413d62116af85bcabfe60d203cb559f4e0285c87
MEAW <meawmobprog@gmail.com>
2019-03-22 10:53:25 +0700

    Fix 4.5

--- app/src/main/java/com/tekkom/meawapp/MainActivity.java
+++ app/src/main/java/com/tekkom/meawapp/MainActivity.java
@@ -1,28 +1,49 @@
 package com.tekkom.meawapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
 
+import com.github.paolorotolo.appintro.AppIntro;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
-        startActivity(new Intent(this, GettingStartedActivity.class));
-//        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-//            startActivity(new Intent(this, LectureMainActivity.class));
-//        } else {
-//            startActivity(new Intent(this, GettingStartedActivity.class));
-//        }
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(this, LectureMainActivity.class));
+        } else {
+            gettingStarted();
+        }
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    void gettingStarted() {
+        addSlide(new GettingStarted1Fragment());
+        addSlide(new GettingStarted2Fragment());
+        showSkipButton(false);
+        showStatusBar(false);
+        setFadeAnimation();
+        //askForPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+//        askForPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+//        askForPermissions(new String[]{Manifest.permission.INTERNET}, 1);
+//        askForPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+
     }
 }
